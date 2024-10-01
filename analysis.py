d95eff3f2f74257c115b417d2579c0816abbcf94
Brendan Doney <bre.doney@gmail.com>
2024-10-01 13:06:53 -0400

    Add rolling average and total time stats

--- analysis.py
+++ analysis.py
@@ -1,16 +1,22 @@
 from collections.abc import Iterable
 import sqlite3
-from typing import Any # type: ignore[reportAny]
+from typing import Any  # type: ignore[reportAny]
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import scipy.stats as stats
 
 from bucketcounter import BucketCounter
 
 plt.style.use("seaborn-v0_8-paper")
 
 conn = sqlite3.connect("./linkdata.db")
-SHOW_GRAPHS = True
+# SHOW_GRAPHS = True
+SHOW_GRAPHS = False
+
+RETRIEVAL_COLS = ["relevance", "helpfulness"]
+LLM_COLS = RETRIEVAL_COLS + ["correctness"]
 
 
 def query_list(query: str) -> list[Any]:
@@ -39,7 +45,7 @@ def query_print_averages(cols: list[str], table: str) -> None:
     query = f"select {avg_str} from {table}"
     res: tuple[float, ...]
     res = conn.execute(query).fetchone()
-    print(f"Average values for {table}")
+    print(f"### Average values for {table}")
     for col, val in zip(cols, res):
         print(f"- {col.title()}: {val}")
 
@@ -178,17 +184,38 @@ query_print(
 print("\n## Reviews")
 print(f"Num reviews: {num_reviews}")
 print(f"├ LLM: {num_llm_reviews} ({num_llm_reviews / num_reviews:.2%})")
-print(f"└ Retreival: {num_retrieval_reviews} ({num_retrieval_reviews / num_reviews:.2%})")
-retrieval_cols = ["relevance", "helpfulness"]
-llm_cols = retrieval_cols + ["correctness"]
+print(
+    f"└ Retreival: {num_retrieval_reviews} ({num_retrieval_reviews / num_reviews:.2%})"
+)
+
+query = """
+select AVG(review_count) from (
+    select COUNT(*) as review_count from llm_reviews group by post_id
+)
+"""
+query_print("Average num reviews per LLM post", query)
+
+query = """
+select AVG(review_count) from (
+    select COUNT(*) as review_count from retrieval_reviews group by post_id
+)
+"""
+query_print("Average num reviews per retrieval post", query)
+
+# Both types of reviews are pretty close to 1 review per post, which makes sense
+# TODO: Check if the author is generally the sole review
+# TODO: Check the average number of non-author reviews
+
 # Average scores for LLM
-query_print_averages(llm_cols, "llm_reviews")
+query_print_averages(LLM_COLS, "llm_reviews")
 # Average scores for retrieval
-query_print_averages(retrieval_cols, "retrieval_reviews")
+query_print_averages(RETRIEVAL_COLS, "retrieval_reviews")
 
 
 def graph_review_count(
-    review_counts: Iterable[int], title: str, buckets: list[tuple[int, int]] | None = None
+    review_counts: Iterable[int],
+    title: str,
+    buckets: list[tuple[int, int]] | None = None,
 ) -> None:
     counter = BucketCounter(review_counts, buckets)
 
@@ -214,7 +241,6 @@ def graph_review_count(
 
 # TODO: Scatter plot of number of questions vs. number of reviews
 # TODO: Scatter plot of number of questions vs. helpfulness, relevance, etc.
-# TODO: Number of reviews on the same post - how distributed are the reviews?
 #       SELECT * from "llm_reviews" GROUP BY post_id having count(*) > 1 LIMIT 200;
 
 query = """
@@ -232,22 +258,25 @@ FROM users u
 WHERE u.consent='TRUE'
 GROUP BY u.id;
 """
-retrieval_reviews: tuple[int, ...]
-llm_reviews: tuple[int, ...]
-total_reviews: tuple[int, ...]
-[retrieval_reviews, llm_reviews, total_reviews] = zip(*query_list(query))
+num_retrieval_reviews_by_user: tuple[int, ...]
+num_llm_reviews_by_user: tuple[int, ...]
+num_total_reviews_by_user: tuple[int, ...]
+[num_retrieval_reviews_by_user, num_llm_reviews_by_user, num_total_reviews_by_user] = (
+    zip(*query_list(query))
+)
 
 for data, name, bucket in [
-    (retrieval_reviews, "retrieval", [(6, 8)]),
-    (llm_reviews, "llm", [(10, 12)]),
-    (total_reviews, "total", [(1, 2), (12,15)]),
+    (num_retrieval_reviews_by_user, "retrieval", [(6, 8)]),
+    (num_llm_reviews_by_user, "llm", [(10, 12)]),
+    (num_total_reviews_by_user, "total", [(1, 2), (12, 15)]),
 ]:
+    print(f"### {name.title()} review info")
     # Number of unique authors who left reviews
-    print(f"Num {name} review authors:", len(data))
+    print(f"- Num authors:", len(data))
 
     # Reviews per author
-    print(f"Average {name} reviews per user:", np.average(data))
-    print(f"Stddev {name} retrieval reviews per user:", np.std(data))
+    print(f"- Average per user:", np.average(data))
+    print(f"- Stddev per user:", np.std(data))
 
     if SHOW_GRAPHS:
         graph_review_count(
@@ -256,9 +285,148 @@ for data, name, bucket in [
             bucket,
         )
 
-num_5plus_users = sum(1 for review in total_reviews if review >= 5)
+num_5plus_users = sum(1 for review in num_total_reviews_by_user if review >= 5)
 print(
-    f"Number of users with 5 or more reviews: {num_5plus_users} ({num_5plus_users / num_users:.2%})"
+    f"Number of users with 5 or more total reviews: {num_5plus_users} ({num_5plus_users / num_users:.2%})"
 )
 
 # What were the other users who have no reviews doing? - They were making posts but not reviewing them
+
+print("\n## Rolling Averages")
+
+llm_df = pd.read_sql(  # pyright: ignore[reportUnknownMemberType]
+    "select * from llm_reviews", con=conn, index_col=["post_id", "author"]
+)
+retrieval_df = pd.read_sql(  # pyright: ignore[reportUnknownMemberType]
+    "select * from retrieval_reviews", con=conn, index_col=["post_id", "author"]
+)
+
+WINDOW_LEN = 100
+for name, df, cols in [
+    ("llm_reviews", llm_df, LLM_COLS),
+    ("retrieval_reviews", retrieval_df, RETRIEVAL_COLS),
+]:
+    averages = []
+    for i in range(len(df) - WINDOW_LEN + 1):
+        rows = df[i : i + WINDOW_LEN][cols]
+        averages.append(rows.mean())
+    window_averages = pd.DataFrame(averages)
+
+    if SHOW_GRAPHS:
+        ax = window_averages.plot(
+            title=f"Rolling Average of {WINDOW_LEN} Reviews",
+            ylabel="Average Value",
+            xlabel="Window Starting Point (Review Index)",
+            xticks=range(
+                0, len(window_averages), WINDOW_LEN
+            ),  # Set xticks every WIDNOW_LEN points
+        )
+
+    print(f"T-test and p-values for {name} rolling averages:")
+    for i, col in enumerate(cols):
+        overall_average = df[col].mean()
+        t_stat, p_value = stats.ttest_1samp(window_averages[col], overall_average)
+        print(
+            f"- {col}: proposed mean: {window_averages[col].mean()} overall mean: {overall_average} -> p-value {p_value}"
+        )
+
+        alternative = "greater"
+        # This combo is the only negative slope
+        if name == "retrieval_reviews" and col == "relevance":
+            alternative = "less"
+
+        linreg = stats.linregress(
+            window_averages.index, window_averages[col], alternative=alternative
+        )
+        print("  ", linreg)
+
+        if SHOW_GRAPHS:
+            color = ax.get_lines()[i].get_color()
+            y = slope * window_averages.index + intercept
+            ax.plot(window_averages.index, y, color=color, linestyle="dashed")
+
+    if SHOW_GRAPHS:
+        plt.show()
+
+# LLM
+# - All have an extremely small p-value, so slope is definitely positive, meaning that students were getting better over time.
+# - The slope of correctness > helpefulness > relevance
+# - The actual averages are correctness > relevance > helpfulness though
+# Retrieval
+# - Again, all have small p-values, so slopes are definitely correct
+# - Slope for relevance is negative, slope for helpfulness is positive
+# - Helpfulness started low, but was matching than relevance by the end
+
+print("\n## Time vs Ratings")
+
+query = """
+    select l.post_id, l.author, l.relevance, l.helpfulness, l.correctness, p.retrieval_time + p.generation_time as total_time
+    from 'llm_reviews' as l
+    inner join posts as p
+        on l.post_id = p.id
+    order by total_time asc
+"""
+llm_gen_df = pd.read_sql(query, conn, index_col=["post_id", "author"])
+
+# Remove the single outlier (network issues)
+llm_gen_df = llm_gen_df[llm_gen_df["total_time"] < 3]
+
+print("### LLM total time info")
+for col in LLM_COLS:
+    x = llm_gen_df["total_time"]
+    linreg = stats.linregress(x, llm_gen_df[col])
+    spearman = stats.spearmanr(x, llm_gen_df[col])
+
+    print("-", col, linreg, spearman)
+
+    if SHOW_GRAPHS:
+        llm_gen_df.plot(
+            x="total_time",
+            y=col,
+            kind="scatter",
+            title=f"Total Retrieval and Generation Time vs. {col.title()}",
+            xlabel="Total Time",
+            ylabel=col.title(),
+        )
+        # NOTE: These lines have no statistical significance and should probably be removed
+        plt.plot(x, linreg.slope * x + linreg.intercept, linestyle="dashed")
+        plt.show()
+
+# So total time doesn't significantly correlate with any of the ratings, which
+# makes sense since longer time doesn't really mean much when we're streaming
+# the result (users won't be dissatisfied; helpfulness), it won't change how
+# the vector database works (relevance), and we cap our answer at a low number
+# of words (500? correctness)
+
+query = """
+    select r.post_id, r.author, r.relevance, r.helpfulness, p.retrieval_time from 'retrieval_reviews' as r
+    inner join posts as p
+        on r.post_id = p.id
+    order by p.retrieval_time asc
+"""
+retrieval_gen_df = pd.read_sql(query, conn, index_col=["post_id", "author"])
+
+print("### Retrieval time info")
+for col in RETRIEVAL_COLS:
+    x = retrieval_gen_df["retrieval_time"]
+    linreg = stats.linregress(x, retrieval_gen_df[col])
+    spearman = stats.spearmanr(x, retrieval_gen_df[col])
+
+    print("-", col, linreg, spearman)
+
+    if SHOW_GRAPHS:
+        retrieval_gen_df.plot(
+            x="retrieval_time",
+            y=col,
+            kind="scatter",
+            title=f"Retrieval Time vs. {col.title()}",
+            xlabel="Retrieval Time",
+            ylabel=col.title(),
+        )
+        # NOTE: These lines have almost no statistical significance and should probably be removed
+        plt.plot(x, linreg.slope * x + linreg.intercept, linestyle="dashed")
+        plt.show()
+
+# So the closest to being statistically significant is helpfulness, which had a
+# p-value of 0.066. But ultimately, none of the lines here are statistically
+# significant, which makes sense for the same reasons as for the LLMs.
